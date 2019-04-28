2208c934b458027dc9435e020faa4c133f026cb4
Mykola Romaniv <mykolaromaniw@gmail.com>
2019-04-28 23:52:16 +0300

    Wrap code in functions (start(), chooseExpences()). Add checkSavings() function. Fix bugs with IE and prompt()

--- HM1/script.js
+++ HM1/script.js
@@ -2,34 +2,40 @@
 
 let money,
     time;
-money = Number(prompt("Ваш бюджет на месяц?"));
-time = prompt("Введите дату в формате YYYY-MM-DD");
+
+function start() {
+    do {
+        money = +prompt("Ваш бюджет на месяц?", '');
+        time = prompt("Введите дату в формате YYYY-MM-DD", '');
+    } while (isNaN(money) || money == "" || money == null); //TODO: secure date    
+}
+
+start();
 
 let appData = {
     budget: money,
     timeData: time,
-    expenses: {
-
-    },
-    optionalExpenses: {
-
-    },
+    expenses: {},
+    optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true
 };
 
-let doneCount = 0;
-do
-{
-    let expName = prompt("Введите обязательную статью расходов в этом месяце");
-    let cost = prompt("Во сколько обойдется?");   
-    
-    if(typeof(expName) != null && expName != "" &&
-        typeof(cost != null) && cost != "") {
-            appData.expenses[expName] =  cost;
+function chooseExpences() {
+    let doneCount = 0;
+    do {
+        let expName = prompt("Введите обязательную статью расходов в этом месяце", '');
+        let cost = +prompt("Во сколько обойдется?", '');
+
+        if (typeof (expName) != null && expName != "" &&
+            typeof (cost) != null && cost != "" && !isNaN(cost)) {
+            appData.expenses[expName] = cost;
             doneCount++;
         }
-}while( doneCount < 2);
+    } while (doneCount < 2);
+}
+
+chooseExpences();
 
 /*for(let doneCount = 0; doneCount < 2; )
 {
@@ -56,21 +62,31 @@ while( doneCount < 2)
         }
 }*/
 
-appData.moneyPerDay = appData.budget / 30;
+appData.moneyPerDay = (appData.budget / 30).toFixed();
 
 alert("Ежедневный бюджет: " + appData.moneyPerDay);
 
-if(appData.moneyPerDay < 100)
-{
+if (appData.moneyPerDay < 100) {
     alert("Минимальный уровень достатка");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000)
-{
-    alert("Средний уровень достатка");   
-} else if (appData.moneyPerDay > 2000)
-{
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    alert("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
     alert("Високий уровень достатка");
 } else {
     alert("Произошла ошибка");
 }
 
-//console.log(appData);
+
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt("Какова сума накоплений?", ''),
+            percent = +prompt("Под какой процент?");
+        
+        appData.monthIncome = save / 100 /12 * percent;
+        alert("Доход в месяц с вашего депощита: " + appData.monthIncome.toFixed(2));
+    }
+}
+
+checkSavings();
+
+console.log(appData);
