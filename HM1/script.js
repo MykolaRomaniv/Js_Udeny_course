25b795e43fbf262c51a08bde81465d4a0a347dd7
Mykola Romaniv <mykolaromaniw@gmail.com>
2019-04-28 13:20:05 +0300

    Done HM2. Added loops, error protection for user input

--- HM1/script.js
+++ HM1/script.js
@@ -18,10 +18,59 @@ let appData = {
     savings: false
 };
 
-let expName = prompt("Введите обязательную статью расходов в этом месяце");
-appData.expenses[expName] = prompt("Во сколько обойдется?");
+let doneCount = 0;
+do
+{
+    let expName = prompt("Введите обязательную статью расходов в этом месяце");
+    let cost = prompt("Во сколько обойдется?");   
+    
+    if(typeof(expName) != null && expName != "" &&
+        typeof(cost != null) && cost != "") {
+            appData.expenses[expName] =  cost;
+            doneCount++;
+        }
+}while( doneCount < 2);
 
-expName = prompt("Введите обязательную статью расходов в этом месяце");
-appData.expenses[expName] = prompt("Во сколько обойдется?");
+/*for(let doneCount = 0; doneCount < 2; )
+{
+    let expName = prompt("Введите обязательную статью расходов в этом месяце");
+    let cost = prompt("Во сколько обойдется?");   
+    
+    if(typeof(expName) != null && expName != "" &&
+        typeof(cost != null) && cost != "") {
+            appData.expenses[expName] =  cost;
+            doneCount++;
+        }
+}*/
 
-alert(appData.budget / 30);
+/*let doneCount = 0;
+while( doneCount < 2)
+{
+    let expName = prompt("Введите обязательную статью расходов в этом месяце");
+    let cost = prompt("Во сколько обойдется?");   
+    
+    if(typeof(expName) != null && expName != "" &&
+        typeof(cost != null) && cost != "") {
+            appData.expenses[expName] =  cost;
+            doneCount++;
+        }
+}*/
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100)
+{
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000)
+{
+    console.log("Средний уровень достатка");   
+} else if (appData.moneyPerDay > 2000)
+{
+    console.log("Високий уровень достатка");
+} else {
+    console.log("Произошла ошибка");
+}
+
+console.log(appData);
